21d388a8b21d3a61ce1bcd23bc4aa4983319969f
yangdd <yzhcode@gmail.com>
2019-03-14 21:57:03 +0800

    find._img.py
    修改了分割函数

--- find_img.py
+++ find_img.py
@@ -5,43 +5,45 @@
  @Email   : 1725457378@qq.com
  @File    : find_img.py
 """
-"""
-寻址 和 做图函数
-"""
+
 import pandas as pd
 import cv2 as cv
 import matplotlib.pyplot as plt
 import shutil
+import os
+import glob
+import time
 
-def allList(list,i):
+"""
+1. 切割数据集
+2. 展示一组图
+"""
+# 切割数据集
+def useList(df, new_df):
     """
-    返回某一集合特定的三个子图地址
-    :param list: 指定集合
-    :param i: 指定号码
-    :return: 三个子图的路径
+    切割数据集
+    :param df: 传入要切割的list
+    :param new_df: 新名称
+    :return:
     """
-    df = pd.read_csv(list)
+    # print(df)
+    for i in range(len(df)):
+        fl = df['id'][i]
+        # 获取文件夹的名称
+        folder = fl[:2]
+        # 完整的文件路径
+        folder_ = '../dataset/af2019-cv-training-20190312/' + folder + '/' + fl
+        img_a = folder_ + '_a.jpg'
+        img_b = folder_ + '_b.jpg'
+        img_c = folder_ + '_c.jpg'
+        src = '../dataset/data_new/' + new_df + '/'
+        shutil.copy(img_a,src)
+        shutil.copy(img_b,src)
+        shutil.copy(img_c,src)
 
-
-    fl = df['id'][i]
-    # 获取文件夹的名称
-    folder = fl[:2]
-    # 完整的文件路径
-    folder_ = './dataset/af2019-cv-training-20190312/' + folder + '/' + fl
-    img_a = folder_ + '_a.jpg'
-    img_b = folder_ + '_b.jpg'
-    img_c = folder_ + '_c.jpg'
-    return img_a,img_b,img_c
 
 
 def drawGroup(img_a,img_b,img_c):
-    """
-    画出三个子图
-    :param img_a: 路径
-    :param img_b: 路径
-    :param img_c: 路径
-    :return: 图
-    """
     plt.figure(1) # 创建第一个画板（figure）
     plt.subplot(211) # 第一个画板的第一个子图
     img_c = plt.imread(img_c) # 显示图片
@@ -58,3 +60,25 @@ def drawGroup(img_a,img_b,img_c):
     plt.show()
 
 
+def get_file_list(file_path):
+    dir_list = os.listdir(file_path)
+    if not dir_list:
+        return
+    else:
+        # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
+        # os.path.getmtime() 函数是获取文件最后修改时间
+        # os.path.getctime() 函数是获取文件最后创建时间
+        dir_list.sort(key=lambda x: os.path.getmtime(os.path.join(file_path, x)))
+
+if __name__ == '__main__':
+    list_train = pd.read_csv( '../dataset/af2019-cv-training-20190312/list_train.csv')
+    list_test = pd.read_csv( '../dataset/af2019-cv-training-20190312/list_test.csv')
+
+    useList(list_train,'train')
+    print('train ok')
+    # get_file_list('../dataset/data_new/train')
+    # print('sorted ok')
+    useList(list_test,'test')
+    print('test ok')
+    # get_file_list('../dataset/data_new/test')
+    # print('sorted ok')
